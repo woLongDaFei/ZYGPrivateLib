42f8d1db77eaa64ebe51cfa26cb688b53485bedb
woLongDaFei <zhaoyongfei2008@163.com>
2020-08-16 11:34:38 +0800

    添加文件

---
+++ ZYGPrivateLib/dev1.h
@@ -0,0 +1,16 @@
+//
+//  dev1.h
+//  ZYGPrivateLib
+//
+//  Created by 老玩童－赵永斐 on 2020/8/16.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface dev1 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZYGPrivateLib/dev1.m
@@ -0,0 +1,12 @@
+//
+//  dev1.m
+//  ZYGPrivateLib
+//
+//  Created by 老玩童－赵永斐 on 2020/8/16.
+//
+
+#import "dev1.h"
+
+@implementation dev1
+
+@end
